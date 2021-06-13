54c3f71069dcae403d9071d98fdc3f3381ce6142
thanhthao0609 <thanhthao0609@gmail.com>
2021-06-13 11:36:29 +0700

    update initialize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-import entity.Demo;
-import restkit.HttpClient;
-import restkit.HttpClientMethod;
-import restkit.HttpResponse;
-
 import java.io.IOException;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        
+
     }
 }
--- src/main/java/entity/Demo.java
+++
@@ -1,9 +0,0 @@
-package entity;
-
-public class Demo {
-    private int time;
-
-    private String tvl;
-
-    private String volume;
-}
